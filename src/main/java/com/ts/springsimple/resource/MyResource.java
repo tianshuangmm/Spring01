d25550ad0afb65a87d6ff9d492662d211db29bfe
ts <15313871502@163.com>
2019-05-20 17:34:09 +0800

    Initial commit

---
+++ src/main/java/com/ts/springsimple/resource/MyResource.java
@@ -0,0 +1,21 @@
+package com.ts.springsimple.resource;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.core.io.Resource;
+
+import java.io.IOException;
+
+public class MyResource implements ApplicationContextAware {
+    private ApplicationContext applicationContext;
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
+    public void resource() throws IOException {
+        Resource resource = applicationContext.getResource("classpath:config.txt");
+        System.out.println(resource.getFilename());
+        System.out.println(resource.contentLength());
+    }
+}
---
+++ src/test/java/com/ts/springsimple/resource/MyResourceTest.java
@@ -0,0 +1,7 @@
+package com.ts.springsimple.resource;
+
+import static org.junit.Assert.*;
+
+public class MyResourceTest {
+
+}
